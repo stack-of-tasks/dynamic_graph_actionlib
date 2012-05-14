6c6dd58beed7ba875851badc13b0234cb7cf3754
Thomas Moulard <thomas.moulard@gmail.com>
2012-05-14 14:25:58 +0200

    Debug ExecuteMotionPlan.

--- src/dynamic_graph/ros/actionlib/server.py
+++ src/dynamic_graph/ros/actionlib/server.py
@@ -1,3 +1,7 @@
+import sys
+if not 'argv' in sys.__dict__:
+  sys.argv = ['na']
+
 import roslib; roslib.load_manifest('dynamic_graph_actionlib')
 import rospy
 import actionlib
@@ -6,22 +10,20 @@ from dynamic_graph_actionlib.msg import *
 
 class ExecuteMotionPlanServer(object):
   def __init__(self, robot, solver):
-      self.robot = robot
-      self.solver = solver
-      self.server = actionlib.SimpleActionServer(
-          'execute_motion_plan', ExecuteMotionPlanAction, self.execute, False)
-      self.server.start()
+    rospy.init_node('execute_motion_plan_server', disable_signals=True)
+    self.robot = robot
+    self.solver = solver
+    self.server = actionlib.SimpleActionServer(
+      'execute_motion_plan', ExecuteMotionPlanAction, self.execute, False)
+    self.server.start()
 
   def execute(self, goal):
-      executeMotionPlanSrv = rospy.ServiceProxy(
-          'execute_motion_plan', ExecuteMotionPlan)
-
-      try:
-          from dynamic_graph.sot.motion_planner.motion_plan import MotionPlan
-          #FIXME: change API to suport reading file from string.
-          motionPlan = MotionPlan(goal.plan, robot, solver, [])
-          motionPlan.start()
-          # FIXME: we should wait for termination here
-          self.server.set_succeeded(actionResult)
-      except e:
-          self.server.set_aborted()
+    try:
+      from dynamic_graph.sot.motion_planner.motion_plan import MotionPlan
+      #FIXME: change API to suport reading file from string.
+      motionPlan = MotionPlan(goal.plan, robot, solver, [])
+      motionPlan.start()
+      # FIXME: we should wait for termination here
+      self.server.set_succeeded(actionResult)
+    except:
+      self.server.set_aborted()
